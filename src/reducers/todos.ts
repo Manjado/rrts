b109e7eea39e8d72d7f75e5419b7fb878fcdf066
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-12-05 09:17:10 +0100

    deleteToDo

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,10 +1,12 @@
-import { Todo, FetchTodosAction } from '../actions';
-import { ActionTypes } from '../actions/types';
+import { Todo, Action, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      //action; // thanks to the guard we know what exactly is in action
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
